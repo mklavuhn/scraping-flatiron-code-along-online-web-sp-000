eba568957f8769bc2a134933d2e84bf319218e50
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-24 20:54:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,8 +28,14 @@ class Scraper
   
   def make_courses
     self.get_courses.each do |post|
+      course = Course.new 
+      course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text 
+      course.description = post.css("p").text 
+    end
+  end
   
-  Scraper.new.get_page
+  Scraper.new.print_courses
   
 end
 
