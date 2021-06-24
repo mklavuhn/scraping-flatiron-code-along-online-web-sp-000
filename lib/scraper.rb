37ec468f3a20dbc2f5c800845732bad37afee8ad
Michael C Klavuhn <michaelklavuhn3@gmail.com>
2021-06-24 20:52:31 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,6 +22,13 @@ class Scraper
     
   end
   
+  def get_courses
+    self.get_page.css(".post")
+  end
+  
+  def make_courses
+    self.get_courses.each do |post|
+  
   Scraper.new.get_page
   
 end
